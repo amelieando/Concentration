352a1817cd4ff8e4f708fba2f5fd822dff658618
whatsGee <gadejesus09@gmail.com>
2022-12-06 20:23:20 -0800

    comet

--- app/src/main/java/com/example/concentration/MainActivity.java
+++ app/src/main/java/com/example/concentration/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,7 +33,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openActivity2() {
-        Intent intent = new Intent(this, cGame.class);
-        startActivity(intent);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, cGame.class));
+            }
+        }, 5000);
+        //Intent intent = new Intent(this, cGame.class);
+        //startActivity(intent);
     }
 }
