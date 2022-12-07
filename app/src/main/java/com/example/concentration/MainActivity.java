e4f41703399b86114725f10e3896b32e67d0a3a2
dmaxc <maxcollegeemail@gmail.com>
2022-12-06 19:03:14 -0800

    Play Button

--- app/src/main/java/com/example/concentration/MainActivity.java
+++ app/src/main/java/com/example/concentration/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.concentration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button playBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playBtn = (Button) findViewById(R.id.playBtn);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, cGame.class);
+        startActivity(intent);
     }
 }
